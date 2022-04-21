49ad5814870b5d2541a477bebc9fa9b53ac5cc56
Zoe <zrodriguez@auntap.com>
2022-04-21 12:08:42 -0300

    get games test added

--- tests/index.js
+++ tests/index.js
@@ -7,19 +7,19 @@ chai.should();
 
 describe('Test API', () => {
 
-    describe('GET/game/:id', () => {
+    const data = {
+        "game": {
+          "id": "M674EZ8NPWi7W7G55PVG5P",
+          "created": "2022-03-22T17:18:26.259101-03:00",
+          "state": {
+            "code": "1",
+            "description": "CREATED"
+          }
+        },
+        "cells": []
+      };
 
-        const data = {
-            "game": {
-              "id": "M674EZ8NPWi7W7G55KVG5P",
-              "created": "2022-03-22T17:18:26.259101-03:00",
-              "state": {
-                "code": "1",
-                "description": "CREATED"
-              }
-            },
-            "cells": []
-          };
+    describe('GET/game/:id', () => {
 
         it('It should find or create a game', (done) => {
             const id = 1234;
@@ -27,13 +27,29 @@ describe('Test API', () => {
             .get(`http://localhost:3000/api/game/${id}`)
             .then(res => {
                 expect(res.data).to.be.an('object');
-                res.should.have.status(200);
                 expect(parseInt(res.data.game.id)).to.be.equal(id);
                 expect(res.data).to.not.be.empty;
                 done()
             })
             .catch(err => done(err));
         })
+        });
+
+    describe('GET/game/:id', () => {
+
+        it('It should return all saved games', (done) => {
+            axios
+            .get(`http://localhost:3000/api/game`)
+            .then(res => {
+                expect(res.data).to.be.an('array');
+                done()
+            })
+            .catch(err => done(err));
+        })
+        });
+
+    describe('POST/game', () => {
+
         it('It should save a new game and return it', (done) => {    
             axios
             .post(`http://localhost:3000/api/game`, data)
@@ -43,6 +59,7 @@ describe('Test API', () => {
             })
             .catch(err => done(err));
         })
+
         it('It should return status 500 if id game already exists', (done) => {
             axios
             .post(`http://localhost:3000/api/game`, data)
